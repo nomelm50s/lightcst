e4021c020eb7a28acea76cece4b83961d13947a1
nomelm50s <nomelm@gmail.com>
2022-01-25 16:04:16 -0500

    Cleaned up extra code

--- src/calclighttime.py
+++ src/calclighttime.py
@@ -2,8 +2,9 @@
 # Synopsis: First draft of calclighttime for Python 3
 # Written by: Mike Lemon
 # Date of Creation: Jan 25, 2022
-# Version: 0.0.1
+# Version: 0.0.2
 # **********************************************************************************************
+#
 import datetime
 import pandas as pd
 
@@ -16,23 +17,10 @@ blend = 0  # Time the back lights are turned off
 ontime = 0  # Temp storage for a total on time
 fronttime = datetime.timedelta()
 backtime = datetime.timedelta()
-
 tms = []
-i = 1
 
 df = pd.read_csv('/home/mikee/Python_proj/lightcst/data/LightStatus.csv') # reads the csv file into a Pandas Data Frame.
-
-dt = df['Date'] # Get a copy of the Date/Time column as a list of strings
-
 format = '%y/%m/%dT%H:%M:%S]' # The date format string used to convert to a date/time object
-
-dtstart = datetime.datetime.strptime(dt[1], format)
-
-dtend = datetime.datetime.strptime(dt[20], format)
-
-dtdelta = dtend - dtstart
-
-print(dtdelta)
 
 # Find the on time periods for the Front lights
 for index, row in df.iterrows():
@@ -46,14 +34,12 @@ for index, row in df.iterrows():
         if flend != 0:
             ontime = flend - flstart
             fronttime = fronttime + ontime
-            print(fronttime)
+            print(ontime,  fronttime)
             frow = ['Front', row['Date'], ontime]
             tms.append(frow)
             flend = 0   # Reset the end point gets it readly for the next loop
 
         p_row = row['State']
-
-# print(tms)
     
 # Find the on time for the Back Lights
 for index, row in df.iterrows():
@@ -72,6 +58,4 @@ for index, row in df.iterrows():
             blend = 0   # Reset the end point to get it ready for the next loop
         
         p_row = row['State']
-
-# print(tms)
 
