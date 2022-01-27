2e9f84dfed0542296bc2f27ce15aac78ff651d8c
nomelm50s <nomelm@gmail.com>
2022-01-27 17:21:03 -0500

    Added checkingontimes  module

--- src/calclighttime.py
+++ src/calclighttime.py
@@ -1,33 +1,31 @@
 # **********************************************************************************************
-# Synopsis: First draft of calclighttime for Python 3
+# Synopsis: calclighttime for Python 3
 # Written by: Mike Lemon
 # Date of Creation: Jan 25, 2022
 # Version: 0.0.2
 # **********************************************************************************************
-#
 import datetime
 import pandas as pd
-
 
 def getontimes(csvfile, switchname):
     """ This fuction collects the sum of ON times from a CSV file and returns them in a list """
     
-    p_row = 0  # holds the previos row values
-    frow = ""  # holds the current row values
-    flstart = 0  # Time the lights are turned on
-    flend = 0  # Time the lights are turned off
+    p_row = 0  # holds the previos row value
+    frow = ""  # holds the current row value
+    flstart = 0  # Time and date the lights are turned on
+    flend = 0  # Time and date the lights are turned off
     ontime = 0  # Temp storage for total ON time
+
     ON = 1
     OFF = 0
-
     datetm = datetime.timedelta()
     tms = []
 
     df = pd.read_csv(csvfile) # reads the csv file into a Pandas Data Frame.
     
-    format = '%y/%m/%dT%H:%M:%S]' # The date format string used to convert to a date/time object
+    format = '%y/%m/%dT%H:%M:%S]' # The date format string used to convert to a datetime object
 
-    # Find each of the ON time periods for the named light switch
+    # Find each of the ON time periods for the switchname
     for index, row in df.iterrows():
         if row['Name'] == switchname:
             if row['State'] == ON and p_row == OFF: # Find the first occurance of ON when OFF for the named switch.
@@ -36,14 +34,16 @@ def getontimes(csvfile, switchname):
             if row['State'] == OFF and p_row == ON: # Find the first occurance of OFF when ON for the named switch.
                 flend = datetime.datetime.strptime(row['Date'], format)
 
-            if flend != 0:
-                ontime = flend - flstart
-                datetm = datetm + ontime
-                frow = [switchname, ontime]
-                tms.append(frow)
-                flend = 0   # Reset the end point gets it readly for the next loop
+            if flend != 0: # The lights are still ON
+                ontime = flend - flstart # delta of ON time
+                datetm = datetm + ontime # Running sum of ON time
+                time = str(ontime) # convert ontime to a formatted string
+                sumtime = str(datetm) # convert summary time to a formatted string
+                frow = [switchname, time, sumtime] # store the current values 
+                tms.append(frow) # create a list of times 
+                flend = 0   # Resets the end point so it's readly for the next loop
 
-            p_row = row['State'] # All done set the previos row to the current.
+            p_row = row['State'] # Done with condition checks, set the previos row to the current.
     
-    return(tms)        
+    return(tms) # returns the list of times        
 
