9f77f05f1020e47a4ba15abaae8fa0089b0d73ab
nomelm50s <nomelm@gmail.com>
2022-02-12 17:14:46 -0500

    renamed modules

--- src/checkingontimes.py
+++ src/checkingontimes.py
@@ -1,23 +1,23 @@
 ''' 
-
-checkingontimes gets the light ON times for each switch called
+checkingontimes gets the total light-ON times for each switch called
 Written by: Mike Lemon
 Date of Creation: Jan 27, 2022
 Version: 0.0.2
-
 '''
-from calclighttime import getontimes 
+from lighttime import ontimes 
 
-# Define location of a data file
+# Defines the location of a data file
+# TODO: make this a pick from file list in gui
 csvfile = '~/projects/lightcst/data/LightStatus.csv'
 
-# Combine all of the measurements in one list by calling getontimes for each light
-ontimes = (getontimes(csvfile, "Front Lights"))
+# Accumulate a list of On-times from the data file for the supplied switch name
+ontimes = (ontimes(csvfile, "Front Lights"))
 
-# calculate the next set of times and concatenates them to the end of the list
-ontimes = ontimes + (getontimes(csvfile, "Back Yard Lights"))
+# Accumulate a list of On-times from the data file for the supplied switch name
+ontimes = ontimes + (ontimes(csvfile, "Back Yard Lights"))
 
-# Writes the list of values to a file
+# Writes a list of summarized On-time values to a file
+# TODO: Send the output to a table or chart in the gui version
 fpath = '/home/mikee/projects/lightcst/data/Lighton.txt'
 with open(fpath, 'w') as filehandle:
     for listitem in ontimes:
