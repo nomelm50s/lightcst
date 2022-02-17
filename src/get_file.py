93fe7d344a1063a4196374dc01f04ac158da62aa
nomelm50s <nomelm@gmail.com>
2022-02-17 09:17:36 -0500

    added alternet methods of finding files and folder

--- src/get_file.py
+++ src/get_file.py
@@ -27,3 +27,31 @@ fn = int(input("\n\r Select log file [0 - " + str(cnt - 1) + "]: "))
 
 print(fileList[fn])
 
+#--------------------------------------------------------------
+# sample function for getting file and folder names
+# import os
+
+# folder_location = '/home/mikee/projects/'
+
+# def create_file_list(path):
+#     return_list = []
+
+#     for filenames in os.walk(path):
+#         for file_list in filenames:
+#             for file_name in file_list:
+#                 if file_name.endswith((".csv")):
+#                     return_list.append(file_name)
+#     print(return_list)
+    
+#     return return_list
+
+# file_list = create_file_list(folder_location)
+#--------------------------------------------------------------
+# Sample method of getting file and folder names
+# path = "/home/mikee/projects/"
+
+# for i in os.scandir(path):
+#     if i.is_file():
+#         print('File: ' + i.path)
+#     elif i.is_dir():
+#         print('Folder: ' + i.path)
