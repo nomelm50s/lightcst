9086414a86adccd7aec529a7e9df661ed36df5c1
nomelm50s <nomelm@gmail.com>
2022-02-28 14:52:56 -0500

    combined 3 folders

---
+++ src/light_time_anal.py
@@ -0,0 +1,25 @@
+from matplotlib import pyplot as plt
+import pandas as pd
+from datetime import date
+from datetime import time
+import re, datetime
+
+df = pd.read_csv('LightStatusc.csv', parse_dates=["Date"])
+
+dts = df["Date"]
+sta = df["Time"]
+dates =[]
+
+for row in dts:
+    match = re.search('\d{2}-\d{2}-\d{2}', row)
+    dates.append(datetime.datetime.strptime(match.group(), '%y-%m-%d').date())
+    print(dates)
+
+
+    match = re.search('\d{2}-\d{2}-\d{2}', row)
+    dates.append(datetime.datetime.strptime(match.group(), '%y-%m-%d %h:%m:%s').date())
+
+# working with dates but not time
+    for row in dts:
+    match = re.search('\d{2}-\d{2}-\d{2}', row)
+    dates.append(datetime.datetime.strptime(match.group(), '%y-%m-%d').date())
